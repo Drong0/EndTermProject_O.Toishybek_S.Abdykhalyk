950e5f49be05ccc88b63c7870f1ee5a706e63475
Drong0 <74701799+Drong0@users.noreply.github.com>
2021-03-07 22:28:30 +0600

    Delete src/com directory

--- src/com/company/data/IDB.java
+++
@@ -1,9 +0,0 @@
-package com.company.data;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IDB {
-    //    Interface of DB connection
-    Connection getConnection() throws SQLException, ClassNotFoundException;
-}
